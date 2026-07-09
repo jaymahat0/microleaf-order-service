a522d055571f516a78043e3f5f0c0b5f7b245eba
jaymahat0 <jaymahato.333@gmail.com>
2026-07-09 09:28:35 +0530

    Implemented Inter Service communication via RestTemplate

--- src/main/java/com/microleaf/order_service/controller/OrderController.java
+++ src/main/java/com/microleaf/order_service/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.microleaf.order_service.controller;
 
 import com.microleaf.order_service.entity.OrderEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,5 +20,16 @@ public class OrderController {
                 OrderEntity.builder().id(1L).product("SAMSUNG GALAXY S24 ULTRA").orderId(UUID.randomUUID()).orderDate(LocalDateTime.of(2026,6,13,13,0)).build(),
                 OrderEntity.builder().id(2L).product("SAMSUNG GALAXY S24 ULTRA").orderId(UUID.randomUUID()).orderDate(LocalDateTime.of(2026,6,15,15,15)).build()
         );
+    }
+
+
+    @GetMapping("/payment/options")
+    public List getAllPaymentOptions() {
+        String url = "http://localhost:8082/payment";
+        RestTemplate restTemplate = new RestTemplate();
+        var re = restTemplate.exchange(url, HttpMethod.GET, null, List.class);
+
+        return re.getBody();
+
     }
 }
